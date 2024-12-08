fa220b6173c4b7e9c1c0503308ab352366186f4b
Kristian Gaardsted Bork <70579756+krisbork@users.noreply.github.com>
2024-12-08 21:29:26 +0100

    Added titles

--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -5,33 +5,35 @@ import FaqItem from "../../components/FaqItem";
 function Faq() {
     return (
         <div className="container faq-item-container">
-            <FaqItem 
-                title="Hvordan opretter jeg min gårdbutik i appen?" 
-                content="Du kan oprette din gårdbutik i appen ved at trykke på plus-ikonet øverst i højre hjørne af appen. Herefter skal du udfylde de nødvendige felter og placere din gårdbutik så nøjagtigt som muligt. Når du har gjort dette, vil din gårdbutik blive sendt til godkendelse, og du vil få en notifikation, når din gårdbutik er blevet godkendt (dog kun hvis du har slået notifikationer til i appen)." />
-            <FaqItem 
-                title="Hvordan opretter jeg en bruger i appen?" 
-                content={"Du kan oprette en bruger i appen ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du udfylde de nødvendige felter og trykke på \"Opret bruger\"."} />
-            <FaqItem 
-                title="Hvordan søger jeg efter Sælv i nærheden af mig?" 
-                content={"Du kan søge efter Sælv i nærheden af dig ved at navigere rundt på kortet og zoome ind og ud, så vil appen automatisk vise dig de gårdbutikker, der er tættest på dig."} />
+            <h1>Ofte stillede spørgsmål</h1>
+            
+            <FaqItem
+                title="Hvordan opretter jeg min gårdbutik i appen?"
+                content="Du kan oprette din gårdbutik i appen ved at trykke på plus-ikonet øverst i højre hjørne af appen. Herefter skal du udfylde de nødvendige felter og placere din gårdbutik så nøjagtigt som muligt. Når du har gjort dette, vil din gårdbutik blive sendt til godkendelse, og du vil få en notifikation, når din gårdbutik er blevet godkendt (dog kun hvis du har slået notifikationer til i appen)."/>
+            <FaqItem
+                title="Hvordan opretter jeg en bruger i appen?"
+                content={"Du kan oprette en bruger i appen ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du udfylde de nødvendige felter og trykke på \"Opret bruger\"."}/>
+            <FaqItem
+                title="Hvordan søger jeg efter Sælv i nærheden af mig?"
+                content={"Du kan søge efter Sælv i nærheden af dig ved at navigere rundt på kortet og zoome ind og ud, så vil appen automatisk vise dig de gårdbutikker, der er tættest på dig."}/>
             <FaqItem
                 title="Hvordan søger jeg efter bestemte varer?"
-                content={"Du kan se om de gårdbutikker, der er tættest på dig, har de varer, du søger, ved at indtaste varenavnet i søgefeltet øverst i appen. Herefter vil appen vise dig de gårdbutikker, der har de varer, du søger."} />
+                content={"Du kan se om de gårdbutikker, der er tættest på dig, har de varer, du søger, ved at indtaste varenavnet i søgefeltet øverst i appen. Herefter vil appen vise dig de gårdbutikker, der har de varer, du søger."}/>
             <FaqItem
                 title="Hvordan kan jeg se, hvilke varer en gårdbutik har?"
-                content={"Du kan se, hvilke varer en gårdbutik har, ved at trykke på gårdbutikken på kortet. Herefter vil appen vise dig en forhåndsvisning af gårdbutikken, hvorefter du kan trykke på \"Se mere\" for at se alle de varer, gårdbutikken har samt se priser og beskrivelser af varerne."} />
+                content={"Du kan se, hvilke varer en gårdbutik har, ved at trykke på gårdbutikken på kortet. Herefter vil appen vise dig en forhåndsvisning af gårdbutikken, hvorefter du kan trykke på \"Se mere\" for at se alle de varer, gårdbutikken har samt se priser og beskrivelser af varerne."}/>
             <FaqItem
                 title="Hvordan opdaterer jeg min Sælv?"
-                content={"Du kan opdatere din Sælv ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du trykke på \"Dine Sælv\" og herefter finde den Sælv, du vil opdatere. Når du har fundet den, skal du trykke på de tre prikker øverst i højre hjørne og herefter trykke \"Rediger Sælv\". Herefter kan du redigere de nødvendige felter og trykke \"Opdater\"."} />
+                content={"Du kan opdatere din Sælv ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du trykke på \"Dine Sælv\" og herefter finde den Sælv, du vil opdatere. Når du har fundet den, skal du trykke på de tre prikker øverst i højre hjørne og herefter trykke \"Rediger Sælv\". Herefter kan du redigere de nødvendige felter og trykke \"Opdater\"."}/>
             <FaqItem
                 title="Hvordan sletter jeg min Sælv?"
-                content={"Du kan slette din Sælv ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du trykke på \"Dine Sælv\" og herefter finde den Sælv, du vil slette. Når du har fundet den, skal du trykke på de tre prikker øverst i højre hjørne og herefter trykke \"Slet Sælv\". Herefter skal du bekræfte, at du vil slette din Sælv."} />
+                content={"Du kan slette din Sælv ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du trykke på \"Dine Sælv\" og herefter finde den Sælv, du vil slette. Når du har fundet den, skal du trykke på de tre prikker øverst i højre hjørne og herefter trykke \"Slet Sælv\". Herefter skal du bekræfte, at du vil slette din Sælv."}/>
             <FaqItem
                 title="Hvis jeg har fundet en Sælv i appen som er forkert, hvad gør jeg så?"
-                content={"Hvis du har fundet en Sælv i appen, som er forkert, kan du rapportere den ved at trykke på de tre prikker øverst i højre hjørne af appen. Herefter skal du trykke på \"Rapporter Sælv\" og beskrive, hvad der er galt. Herefter vil vi undersøge sagen og fjerne den, hvis den ikke længere eksisterer."} />
+                content={"Hvis du har fundet en Sælv i appen, som er forkert, kan du rapportere den ved at trykke på de tre prikker øverst i højre hjørne af appen. Herefter skal du trykke på \"Rapporter Sælv\" og beskrive, hvad der er galt. Herefter vil vi undersøge sagen og fjerne den, hvis den ikke længere eksisterer."}/>
             <FaqItem
                 title="Hvordan sletter jeg min bruger?"
-                content={"Du kan slette din bruger ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du trykke på \"Slet din bruger\" og herefter bekræfte ved at trykke på \"Slet bruger\". Dette vil slette din bruger og alle dine data fra appen samt alle dine Sælv."} />
+                content={"Du kan slette din bruger ved at trykke på \"æ\"-ikonet øverst i højre hjørne af appen. Herefter skal du trykke på \"Slet din bruger\" og herefter bekræfte ved at trykke på \"Slet bruger\". Dette vil slette din bruger og alle dine data fra appen samt alle dine Sælv."}/>
         </div>
     );
 }
--- src/pages/om/Om.tsx
+++ src/pages/om/Om.tsx
@@ -4,25 +4,32 @@ import "./Om.css";
 function Om() {
     return (
         <div className="container om-container">
-            <p>Sælv er appen til at finde og udforske tillidsstativer, gårdbutikker, vejboder og meget mere! Uanset om du søger friske råvarer, unikke håndlavede varer, genbrugsskatte eller lokalt producerede specialiteter, har vi gjort det nemt for dig at opdage og støtte lokale producenter.</p>
-
+            <h1>Om Sælv</h1>
+            
+            <p>Sælv er appen til at finde og udforske tillidsstativer, gårdbutikker, vejboder og meget mere! Uanset om
+                du søger friske råvarer, unikke håndlavede varer, genbrugsskatte eller lokalt producerede specialiteter,
+                har vi gjort det nemt for dig at opdage og støtte lokale producenter.</p>
 
             <h3>Funktioner:</h3>
             <ul>
                 <li>
-                    Interaktivt kort: Udforsk et detaljeret kort med markeringer for tillidsstativer, gårdbutikker, vejboder og andre lokale salgssteder i dit område.
+                    Interaktivt kort: Udforsk et detaljeret kort med markeringer for tillidsstativer, gårdbutikker,
+                    vejboder og andre lokale salgssteder i dit område.
                 </li>
                 <li>
-                    Detaljeret information: Klik på en markering for at få detaljerede oplysninger om, hvad der bliver solgt og priser.
+                    Detaljeret information: Klik på en markering for at få detaljerede oplysninger om, hvad der bliver
+                    solgt og priser.
                 </li>
                 <li>
                     Rutevejledning: Få præcis vejvisning til din valgte Sælv direkte fra appen.
                 </li>
                 <li>
-                    MobilePay integration: Har du et MyShop-nummer? Ingen problemer! Vi gør det muligt at brugere nemt kan regne ud hvor meget de skal af med og videresende dem til din MyShop i MobilePay.
+                    MobilePay integration: Har du et MyShop-nummer? Ingen problemer! Vi gør det muligt at brugere nemt
+                    kan regne ud hvor meget de skal af med og videresende dem til din MyShop i MobilePay.
                 </li>
                 <li>
-                    Fokus på genbrug: Find steder, der sælger genbrugsting og gør en forskel ved at støtte bæredygtighed og miljøvenlige initiativer.
+                    Fokus på genbrug: Find steder, der sælger genbrugsting og gør en forskel ved at støtte bæredygtighed
+                    og miljøvenlige initiativer.
                 </li>
             </ul>
 
@@ -32,18 +39,22 @@ function Om() {
                     Søg: Brug vores søgefunktion til at finde specifikke varer eller genbrugsskatte
                 </li>
                 <li>
-                    Udforsk: Se hvilke tillidsstativer, gårdbutikker, vejboder og genbrugsbutikker der er i nærheden af dig.
+                    Udforsk: Se hvilke tillidsstativer, gårdbutikker, vejboder og genbrugsbutikker der er i nærheden af
+                    dig.
                 </li>
                 <li>
-                    Opdag: Klik på et sted for at få detaljeret information om, hvad der tilbydes, priser og hvordan du finder derhen.
+                    Opdag: Klik på et sted for at få detaljeret information om, hvad der tilbydes, priser og hvordan du
+                    finder derhen.
                 </li>
                 <li>
-                    Betal: Hvis salgsstedet har et MyShop-nummer, kan du betale hurtigt og sikkert via MobilePay eller ved hjælp af de andre betalingsmuligheder i appen.
+                    Betal: Hvis salgsstedet har et MyShop-nummer, kan du betale hurtigt og sikkert via MobilePay eller
+                    ved hjælp af de andre betalingsmuligheder i appen.
                 </li>
             </ul>
-            
+
             <h3>Hvorfor Sælv?</h3>
-            <p><b>Støt Lokalt, bidrag til din lokale økonomi ved at handle direkte fra lokale producenter, der sælger friske råvarer og unikke produkter, som du ikke finder i supermarkedet.</b></p>
+            <p><b>Støt Lokalt, bidrag til din lokale økonomi ved at handle direkte fra lokale producenter, der sælger
+                friske råvarer og unikke produkter, som du ikke finder i supermarkedet.</b></p>
         </div>
     );
 }
