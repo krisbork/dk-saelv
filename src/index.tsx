fa5c9a1683e795f7f4be5b2422df6940b6e774c3
Kristian Gaardsted Bork <70579756+krisbork@users.noreply.github.com>
2024-12-10 20:11:26 +0100

    Fixed indexing of pages

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import ErrorPage from "./pages/error/ErrorPage";
 import {routes} from "./routes/routes";
+import {hydrate, render} from "react-dom";
 
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  rootElement as HTMLElement
 );
 
 const router = createBrowserRouter([
@@ -20,11 +22,22 @@ const router = createBrowserRouter([
     }
 ])
 
-root.render(
-    <React.StrictMode>
+// @ts-ignore
+if (rootElement.hasChildNodes()) {
+    hydrate(<React.StrictMode>
         <RouterProvider router={router} />
-    </React.StrictMode>
-);
+    </React.StrictMode>, rootElement);
+} else {
+    render(<React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>, rootElement);
+}
+
+// root.render(
+//     <React.StrictMode>
+//         <RouterProvider router={router} />
+//     </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
