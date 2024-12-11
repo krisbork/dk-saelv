b5416c655b690d73f545eb29acf61b850ecb5894
Kristian Gaardsted Bork <70579756+krisbork@users.noreply.github.com>
2024-12-11 12:18:10 +0100

    Added pre-register button for Google Play Store

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,8 @@ import logo from "../../assets/images/saelv-app-logo.png";
 import apppic1 from "../../assets/images/apppic1_v2.webp";
 import apppic2 from "../../assets/images/apppic3_v2.webp";
 import apppic3 from "../../assets/images/apppic4.webp";
+import apple_download from "../../assets/buttons/apple_black.svg";
+import google_preregister from "../../assets/buttons/google_preregister.svg";
 
 function Home() {
     return (
@@ -12,12 +14,12 @@ function Home() {
             <h2 className="details-text">Din guide til tillidsstativer, gårdbutikker, vejboder og genbrug</h2>
             <div className="download-container">
                 <div className="download-buttons">
-                    <a href="https://apps.apple.com/dk/app/s%C3%A6lv/id6504734959"><img className="bn46"
-                                     src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+                    <a href="https://apps.apple.com/dk/app/s%C3%A6lv/id6504734959" target="_blank" rel="noopener noreferrer"><img className="bn46"
+                                     src={apple_download}
                                      alt="bn45"/>
                     </a>
-                    <a href="/" className="disabled-btn"><img className="bn45"
-                                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png"
+                    <a href="https://play.google.com/store/apps/details?id=dk.saelv.android" target="_blank" rel="noopener noreferrer"><img className="bn45"
+                                     src={google_preregister}
                                      alt="bn45"/>
                     </a>
                 </div>
